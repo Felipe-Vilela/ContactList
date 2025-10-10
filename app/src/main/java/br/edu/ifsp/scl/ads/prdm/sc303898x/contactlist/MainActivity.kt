300a82d3cbc696bf7c7798b554e9a2d87e156dba
Felipe Vilela <felipe.vilela01@outlook.com>
2025-10-10 19:58:24 -0300

    cleanup: cleaning MainActivity

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/contactlist/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/contactlist/MainActivity.kt
@@ -1,14 +1,17 @@
 package br.edu.ifsp.scl.ads.prdm.sc303898x.contactlist
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.prdm.sc303898x.contactlist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val amb: ActivityMainBinding by lazy (){
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(amb.root)
     }
 }
