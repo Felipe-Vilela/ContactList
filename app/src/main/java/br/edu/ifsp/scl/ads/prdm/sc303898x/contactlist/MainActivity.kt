65c7f35f45a7a89a0584eebca925299529bfbcf9
Felipe Vilela <felipe.vilela01@outlook.com>
2025-10-10 20:55:55 -0300

    feat: create and use data class Contact

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/contactlist/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/contactlist/MainActivity.kt
@@ -1,17 +1,51 @@
 package br.edu.ifsp.scl.ads.prdm.sc303898x.contactlist
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.ads.prdm.sc303898x.contactlist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val amb: ActivityMainBinding by lazy (){
+    private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    //Data source
+    private val  contactList: MutableList<Contact> = mutableListOf()
+
+    //Adapter
+    private val contactAdapter: ArrayAdapter<String> by lazy {
+        ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            contactList.map { "${it.name} - ${it.phone}" }
+        )
+
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        fillContactList()
+
+        amb.contactTv.adapter = contactAdapter
     }
+
+    private fun fillContactList(){
+        for (i in 1 .. 50){
+            contactList.add(
+                Contact(
+                    i,
+                    "Name: $1",
+                    "Address: $1",
+                    "Phone: $1",
+                    "Email: $1",
+                    )
+                )
+        }
+    }
+
 }
